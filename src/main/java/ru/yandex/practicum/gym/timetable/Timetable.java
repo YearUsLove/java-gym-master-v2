c611f4e63e699db872a6bb5aeac78aa645fae14e
Yaroslav <ypetrunin@nota.tech>
2026-06-16 12:14:11 +0300

    Fix getTrainingSessionsForDay complexity

--- src/main/java/ru/yandex/practicum/gym/timetable/Timetable.java
+++ src/main/java/ru/yandex/practicum/gym/timetable/Timetable.java
@@ -1,16 +1,21 @@
 package ru.yandex.practicum.gym.timetable;
 
 import ru.yandex.practicum.gym.model.*;
+
 import java.util.*;
 
 public class Timetable {
 
-    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable;
+    private final Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable;
+    private final Map<DayOfWeek, List<TrainingSession>> sessionsByDay;
 
     public Timetable() {
         timetable = new EnumMap<>(DayOfWeek.class);
+        sessionsByDay = new EnumMap<>(DayOfWeek.class);
+
         for (DayOfWeek day : DayOfWeek.values()) {
             timetable.put(day, new TreeMap<>());
+            sessionsByDay.put(day, new ArrayList<>());
         }
     }
 
@@ -19,54 +24,63 @@ public class Timetable {
         TimeOfDay time = trainingSession.getTimeOfDay();
 
         TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(day);
+        daySchedule.computeIfAbsent(time, key -> new ArrayList<>()).add(trainingSession);
 
-        if (!daySchedule.containsKey(time)) {
-            daySchedule.put(time, new ArrayList<>());
-        }
-
-        daySchedule.get(time).add(trainingSession);
+        List<TrainingSession> daySessions = sessionsByDay.get(day);
+        int insertIndex = findInsertIndex(daySessions, trainingSession);
+        daySessions.add(insertIndex, trainingSession);
     }
 
     public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
-        List<TrainingSession> result = new ArrayList<>();
-
-        for (TimeOfDay time : daySchedule.navigableKeySet()) {
-            result.addAll(daySchedule.get(time));
-        }
-
-        return result;
+        return Collections.unmodifiableList(sessionsByDay.get(dayOfWeek));
     }
 
     public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(dayOfWeek);
-
         List<TrainingSession> sessions = daySchedule.get(timeOfDay);
 
-        return sessions != null ? new ArrayList<>(sessions) : new ArrayList<>();
+        if (sessions == null) {
+            return Collections.emptyList();
+        }
+
+        return Collections.unmodifiableList(sessions);
     }
 
     public Map<Coach, Integer> getCountByCoaches() {
         Map<Coach, Integer> coachCounts = new HashMap<>();
 
-        for (DayOfWeek day : DayOfWeek.values()) {
-            TreeMap<TimeOfDay, List<TrainingSession>> daySchedule = timetable.get(day);
-            for (List<TrainingSession> sessions : daySchedule.values()) {
-                for (TrainingSession session : sessions) {
-                    Coach coach = session.getCoach();
-                    coachCounts.merge(coach, 1, Integer::sum);
-                }
+        for (List<TrainingSession> daySessions : sessionsByDay.values()) {
+            for (TrainingSession session : daySessions) {
+                coachCounts.merge(session.getCoach(), 1, Integer::sum);
             }
         }
 
         List<Map.Entry<Coach, Integer>> entries = new ArrayList<>(coachCounts.entrySet());
-        entries.sort((e1, e2) -> e2.getValue().compareTo(e1.getValue()));
+        entries.sort((entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
 
-        Map<Coach, Integer> sortedResult = new LinkedHashMap<>();
+        Map<Coach, Integer> result = new LinkedHashMap<>();
         for (Map.Entry<Coach, Integer> entry : entries) {
-            sortedResult.put(entry.getKey(), entry.getValue());
+            result.put(entry.getKey(), entry.getValue());
         }
 
-        return sortedResult;
+        return result;
+    }
+
+    private int findInsertIndex(List<TrainingSession> daySessions, TrainingSession newSession) {
+        int left = 0;
+        int right = daySessions.size();
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            TrainingSession middleSession = daySessions.get(middle);
+
+            if (middleSession.getTimeOfDay().compareTo(newSession.getTimeOfDay()) <= 0) {
+                left = middle + 1;
+            } else {
+                right = middle;
+            }
+        }
+
+        return left;
     }
 }
